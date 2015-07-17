09a2b1f1687420a4a1b85daa2b4776160e2742c1
Olivia Cheng <olivia@heyitsolivia.com>
2015-07-17 00:27:08 -0400

    add base files

---
+++ application.js
@@ -0,0 +1,17 @@
+var MOVIE_PHRASES = [
+    'U HAVE TREAD<br>UPON MY DOMAIN &<br>MUST NOW SUFFER<br>WHO R U ?',
+    'ACID BURN<br>SEZ LEAVE B 4<br>U R EXPUNGED',
+    'I WILL<br>SWAT U LIKE<br>THE FLY U R',
+    'I WILL<br>SNAP YOUR BACK<br>LIKE A TOOTHPICK',
+    'YOU ARE<br>TERMINATED'
+];
+
+var phraseCounter = 0;
+function changePhrase() {
+    var p = document.getElementById('hackertext');
+    var phraseCount = MOVIE_PHRASES.length;
+    p.innerHTML = MOVIE_PHRASES[phraseCounter % phraseCount];
+    phraseCounter = phraseCounter + 1;
+};
+
+window.setInterval(changePhrase, 600);
